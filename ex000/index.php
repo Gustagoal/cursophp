9e289cf91cca02fe7f961ed188a06edc42726b63
Gustagoal <gustavocastruu@gmail.com>
2025-08-23 10:52:12 -0300

    Primeiro arquivo em php

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro programa em PHP</title>
+</head>
+<body>
+    <h1>
+        Curso em PHP
+    </h1>
+    <p>
+        <?php 
+        print 'Olá mundo , teste em php';
+        ?>
+    </p>
+</body>
+</html>
